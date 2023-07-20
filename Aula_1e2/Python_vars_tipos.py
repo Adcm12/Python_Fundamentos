f768cec8ae7da87e9882edfbe9c0553233b46c9b
Adrian Castillo <david.marcanao07@gmail.com>
2023-07-19 22:24:45 -0300

    upload

---
+++ Aula_1e2/Python_vars_tipos.py
@@ -0,0 +1,36 @@
+#Realizando importaciones de bibliotecas
+#Por padrao, elas seran importadas en el comienzo
+#do archivos/programas.py
+
+import os
+
+# 3Sirve para enviar comando al terminal entre otras funciones
+
+os.system('cls')
+
+# os.system('dir')
+
+# # taskill serve para parar el archivo responsable 
+# # para rodar una aplicacion el sistema
+
+
+# os.system('taskkill /f /im brave.exe') 
+
+numero_entero1: int = 5
+numero_entero2 = 89
+numero_entero3 = 500
+
+nome1: str = "\nAdrian"
+nome: str = "\nCastillo"
+texto = '\nJAJAJA'
+
+print(f'Datos de las variables de tipo int, \n', numero_entero1,'\n', numero_entero2, '\n', numero_entero3)
+print(f'\nDatos de las variables de tipo str {nome}, {nome1}, {texto}')
+
+salario: float = 15.56
+
+print(f'\nDatos de las variables de tipo float\n{salario}')
+
+verdade: bool = True
+
+print(f'\nDatos de las variables de tipo bool\n {verdade}')
