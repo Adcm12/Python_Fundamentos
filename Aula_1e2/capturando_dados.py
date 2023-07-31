0378f40d069e7634e24aa4664ef7a8e413a0f771
Adrian Castillo <david.marcanao07@gmail.com>
2023-07-31 19:16:47 -0300

    upload

---
+++ Aula_1e2/capturando_dados.py
@@ -0,0 +1,16 @@
+import os
+
+
+
+if __name__ == '__main__':
+    os.system('cls')
+
+    #Lendo dados do usuario
+    Texto = input('Informe un texto: ')
+    print('Voce escrveu: ', Texto)
+
+    inteiro = input('\nInforme un entero: ')
+    print('Voce informou: ', inteiro)
+
+    float = input('\nInforme un float: ')
+    print('Voce informou: ', float)
---
+++ Aula_1e2/exibindo_message.py
@@ -0,0 +1,20 @@
+import os 
+
+os.system('cls')
+
+def mostrar_dados():
+
+    print('numero: ', 5)
+    print('String:', ' Hola')
+    print('Float: ', 5.2)
+    print('Boolean: ', True)    
+
+
+# Solo va a rodar la funcion que llama al final (sirve para hacer testes sin llamar todo el codigo)
+if __name__ == '__main__':
+
+    os.system('cls')
+
+    mostrar_dados()
+
+
