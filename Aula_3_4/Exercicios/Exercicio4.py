9233ba5210a51f1e36da5dd47fdb1c9d3aa8a1cf
Adrian Castillo <david.marcanao07@gmail.com>
2023-08-09 21:48:11 -0300

    upload

--- Aula_3_4/Exercicios/Exercicio4.py
+++ Aula_3_4/Exercicios/Exercicio4.py
@@ -3,39 +3,49 @@
 
 class Agenda:
     
-    def __init__(self):
-        self.contatos = []
+    contatos = []
     
-    def adicionar_contato(self, nome, telefone):
-        contato = {'nome': nome, 'telefone': telefone}
-        self.contatos.append(contato)
-        print(f"Contato {nome} adicionado com sucesso.")
+    def adicionar_contato(item_lista):
+        Agenda.contatos.append([item_lista])
+        print('Adicionado con sucesso')
     
-    def remover_contato(self, nome):
-        contatos_restantes = [contato for contato in self.contatos if contato['nome'] != nome]
-        if len(contatos_restantes) < len(self.contatos):
-            self.contatos = contatos_restantes
-            print(f"Contato {nome} removido com sucesso.")
-        else:
-            print(f"Contato {nome} não encontrado na agenda.")
+    def remover_contato(posicion):
+        Agenda.contatos.pop(posicion)
+        print('Removido con sucesso')
     
-    def exibir_contatos(self):
-        if self.contatos:
-            print("Lista de contatos:")
-            for contato in self.contatos:
-                print(f"Nome: {contato['nome']}, Telefone: {contato['telefone']}")
-        else:
-            print("A agenda está vazia.")
+    def exibir_contatos():
+        
+        if len(Agenda.contatos) > 0:
 
-# Criando uma instância da classe Agenda
-minha_agenda = Agenda()
+            for i, valor in range(len(Agenda.contatos)):
+                print(f'Posicion: {i}  Nombre: {Agenda.contatos[i][0]}  Telefone: {Agenda.contatos[i][1]}')
 
-# Testando os métodos
-minha_agenda.adicionar_contato("Alice", "123456789")
-minha_agenda.adicionar_contato("Bob", "987654321")
-minha_agenda.exibir_contatos()
+    def script():
+        while True:
 
-minha_agenda.remover_contato("Alice")
-minha_agenda.exibir_contatos()
+            try:
+                metodo = input('Informe el metodo: ')
 
-minha_agenda.remover_contato("Charlie")  # Tentando remover um contato que não existe
+                if metodo == 'adicionar':
+                    novo_item = input('Digite o item a ser adicionado na agenda: ')
+                    novo_numero =int(input('Adicione o telefone: '))
+                    Agenda.adicionar_contato([Agenda.item_lista])
+
+                elif metodo == 'remover':
+
+                    posicion = int(input('informe la poscion: '))
+                    Agenda.remover_contato(posicion)
+
+                elif metodo == 'exibir':
+
+                    Agenda.exibir_contatos()
+
+                elif metodo == 's':
+
+                    break
+
+
+            except Exception as erro:
+                print('error')
+
+Agenda.script()
---
+++ Aula_3_4/getters_setters.py
@@ -0,0 +1,74 @@
+class Animal():
+
+    # Constructor
+
+    # PRIVATE -> __nome
+    # Protected -> _nome
+    # Public
+
+    def __init__(self, raza, cor, edad, nome):
+
+        self._raza = raza
+        self._cor = cor
+        self._edad = edad
+        self.__nome = nome
+        print('Objetos instanciados con sucesso')
+
+
+    #Getters
+    @property
+    def razas(self):
+
+        return '\nRaza: '+ self._raza
+    
+    @property
+    def cor(self):
+
+        return '\nCor: '+self._cor
+    
+    @property
+    def edad(self):
+
+        return f'\nEdad: {self._edad}'
+    
+
+    @property
+    def nome(self):
+
+        return f'\nNome: {self.__nome}'
+    
+
+    #Setters
+    @razas.setter
+    def raza(self, raza_nova):
+
+        print(f'Setou de {self.raza}, para {raza_nova}')
+        self._raza = raza_nova
+
+    @cor.setter
+    def cor(self, cor_novo):
+        print(f'Setou de {self.cor}, para {cor_novo}')
+        self._cor = cor_novo
+
+    @edad.setter
+    def edad(self, edad_nova):
+
+        print(f'Setou de {self.edad}, para {edad_nova}')
+        self._edad = edad_nova
+
+    @nome.setter
+    def nome(self, nome_nova):
+
+        print(f'Setou de {self.__nome}, para {nome_nova}')
+        self.__nome = nome_nova
+
+
+leon = Animal('Leon', 'preto', 15, 'Simba')
+gato = Animal('gato', 'beige', 9, 'Garfield')
+pantera = Animal('pantera', 'cinza', 10, 'Tachala')
+
+print(leon.razas, leon.cor, leon.edad, leon.nome)
+print(gato.razas, gato.cor, gato.edad, gato.nome)
+print(pantera.razas, pantera.cor, pantera.edad, pantera.nome)
+
+print(pantera.nome)
