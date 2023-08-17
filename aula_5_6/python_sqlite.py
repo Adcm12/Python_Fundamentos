7d144da157ca644cedf1be4d8a2d496cb2c14ce8
Adrian Castillo <david.marcanao07@gmail.com>
2023-08-17 17:25:06 -0300

    upload

--- aula_5_6/python_sqlite.py
+++ aula_5_6/python_sqlite.py
@@ -1,121 +1,140 @@
 import sqlite3
 import os
-#Crear class para manipular el bd
-
-    #Crear conexion
 
 class BancoDeDados():
+    conexion = None
 
-    criar_conexion = None
-
-    @staticmethod # Permite accesar metodo sin crear objetos
+    @staticmethod
     def crear_conexion(nome_base):
         BancoDeDados.conexion = sqlite3.connect(nome_base)
-        print('Conexion establecida!')
+        print('Conexión establecida!')
         return BancoDeDados.conexion
 
-    
-    @staticmethod # Permite accesar metodo sin crear objetos
+    @staticmethod
     def crear_tabela(nome_tabela_nova):
         cursor = BancoDeDados.conexion.cursor()
         
         sql = f'''
-        CREATE TABLE if not exists {nome_tabela_nova} (\n
-            id INTEGER PRIMARY KEY,\n
+        CREATE TABLE IF NOT EXISTS {nome_tabela_nova} (
+            id INTEGER PRIMARY KEY,
             nome TEXT (40),
             numero TEXT (15),
             email TEXT (40)); '''
 
         cursor.execute(sql)
         cursor.close()
-        print(f'\nTabel nova creada con exito!')
+        print(f'\n¡Tabla nueva creada con éxito!')
 
-
-    @staticmethod # Permite accesar metodo sin crear objetos
+    @staticmethod
     def inserir_dados(nome_tabela, nome_usu, numero_usu, email_usu):
         cursor = BancoDeDados.conexion.cursor()
 
         sql = f""" 
-                insert into {nome_tabela}\
-                (nome, numero, email)\
-                values ('{nome_usu}', '{numero_usu}', '{email_usu}' )
+                INSERT INTO {nome_tabela}
+                (nome, numero, email)
+                VALUES (?, ?, ?)
                 """
-        cursor.execute(sql)
+        cursor.execute(sql, (nome_usu, numero_usu, email_usu))
         BancoDeDados.conexion.commit()
         cursor.close()
-        print(f'\nDatos inseridos en {nome_tabela} con exito')
+        print(f'\nDatos insertados en {nome_tabela} con éxito')
 
-    @staticmethod # Permite accesar metodo sin crear objetos
-    def deleta_linha(id_linha):
-        pass
+    @staticmethod
+    def deleta_linha(id_linha, nome_tabela):
+        cursor = BancoDeDados.conexion.cursor()
+        
+        sql = f''' 
+        DELETE FROM {nome_tabela}
+        WHERE id = ?;
+        '''
+        cursor.execute(sql, (id_linha,))
+        BancoDeDados.conexion.commit()
+        cursor.close()
+        print('ID eliminado con éxito')
 
-    @staticmethod # Permite accesar metodo sin crear objetos
+    @staticmethod
     def mostar_tabela(nome_tabela):
-        pass
+        cursor = BancoDeDados.conexion.cursor()
+        cursor.execute(f'SELECT * FROM {nome_tabela}')
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row)
+        cursor.close()
 
+    @staticmethod
+    def actualiza_linha(nombre_tabla, id_linha, nome_novo, numero_novo, email_novo):
+        cursor = BancoDeDados.conexion.cursor()
+        sql = f'''
+        UPDATE {nombre_tabla}
+        SET nome = ?, numero = ?, email = ?
+        WHERE id = ?;
+        '''
+        cursor.execute(sql, (nome_novo, numero_novo, email_novo, id_linha))
+        BancoDeDados.conexion.commit()
+        print('\nDatos actualizados con exito!')
+        cursor.close()
 
-    @staticmethod # Permite accesar metodo sin crear objetos
-    def actualiza_linha(id_linha, nome_novo, numero_novo, email_novo):
-        pass
-    
+    @staticmethod
     def database_manager():
-
         conn = BancoDeDados.crear_conexion('base_de_datos.db')
 
         menu = '''
-                \n1 - Crear tabela
-                \n2 - Inserir datos
-                \n3 - Deletar linha
-                \n4 - Mostrar tabela
-                \n5 - Actualizar linha
+                \n1 - Crear tabla
+                \n2 - Insertar datos
+                \n3 - Eliminar línea
+                \n4 - Mostrar tabla
+                \n5 - Actualizar línea
                 \n6 - Salir: '''
-        while True :
+        
+        while True:
             try:
-                os.system('cls')
-                operacio = int(input(menu))
+                operacion = int(input(menu))
 
-                if operacio == 1:
+                if operacion == 1:
+                    nombre_tabla = input('Ingrese el nombre de la nueva tabla: ')
+                    BancoDeDados.crear_tabela(nombre_tabla)
 
-                    nome_tabela = input('Informe el nombre de la tabela nueva: ')
+                elif operacion == 2:
+                    tabla = input('\nIngrese el nombre de la tabla: ')
+                    nombre_usu = input('\nIngrese el nombre: ')
+                    numero_usu = input('Ingrese el número: ')
+                    email_usu = input('Ingrese el correo electrónico: ')
 
-                    BancoDeDados.crear_tabela(nome_tabela)
+                    if len(nombre_usu) >= 3 and 11 <= len(numero_usu) <= 13 and '@' in email_usu and '.com' in email_usu:
+                        print(f'\nNombre {nombre_usu}, número {numero_usu}, y correo electrónico {email_usu} son datos válidos')
+                        BancoDeDados.inserir_dados(tabla, nombre_usu, numero_usu, email_usu)
+                    else:
+                        print('\nPor favor ingrese datos válidos')
 
-                elif operacio == 2:
+                elif operacion == 3:
+                    nombre_tabla = input('Ingrese el nombre de la tabla: ')
+                    id_eliminar = int(input('Ingrese el ID de la línea a eliminar: '))
+                    BancoDeDados.deleta_linha(id_eliminar, nombre_tabla)
 
-                    tabela = input('Informe el nombre de la tabela: ')
-                    nome_usu = input('Informe el nombre: ')
-                    numero_usu = input('Informe el numero: ')
-                    email_usu = input('Informe el email: ')
+                elif operacion == 4:
+                    nombre_tabla = input('Ingrese el nombre de la tabla: ')
+                    BancoDeDados.mostar_tabela(nombre_tabla)
 
-                    BancoDeDados.inserir_dados(nome_tabela= tabela, nome_usu=nome_usu, numero_usu=numero_usu, email_usu= email_usu)
-                    
+                elif operacion == 5:
+                    nombre_tabla = input('Ingrese el nombre de la tabla: ')
+                    id_actualizar = int(input('Ingrese el ID de la línea a actualizar: '))
+                    nombre_nuevo = input('Ingrese el nuevo nombre: ')
+                    numero_nuevo = input('Ingrese el nuevo número: ')
+                    email_nuevo = input('Ingrese el nuevo correo electrónico: ')
+                    BancoDeDados.actualiza_linha(nombre_tabla, id_actualizar, nombre_nuevo, numero_nuevo, email_nuevo)
 
-                elif operacio == 3:
-                    pass                    
+                elif operacion == 6:
+                    break
 
-                elif operacio == 4:
-                    pass
-
-                elif operacio == 5:
-                    pass
-
-                elif operacio == 6:
-
-                    quit()
-                
-                else: 
-
-                    print('Informe una opcion valida')
+                else:
+                    print('Ingrese una opción válida')
 
             except Exception as e:
-                print(f'Ocurrio un error: {str(e)}')
-    
-        conn.close()
+                print(f'Ocurrió un error: {str(e)}')
 
-#Crear funcion main, que sera la interface del usuario
+        conn.close()
+        print('Conexión cerrada')
 
 if __name__ == '__main__':
-
-    os.system('cls')
+    os.system('cls' if os.name == 'nt' else 'clear')
     BancoDeDados.database_manager()
-
