a050e4af26a2df2cba7efbf3de2d2a4ab8f285d2
Adrian Castillo <david.marcanao07@gmail.com>
2023-08-28 19:15:02 -0300

    upload

--- aulas_7_8/review.py
+++ aulas_7_8/review.py
@@ -2,7 +2,7 @@ import os
 
 def eh_positivo(numero):
     
-    if numero % 2 == 0:
+    if numero > 0:
 
         print('Es positivo')
 
@@ -20,13 +20,40 @@ def son_iguales(numero1, numero2):
 
         print('No son iguales')
 
+def main():
 
+    menu = '''
+    \nSeleccione la funcion:
+    \n1) Es positivo:
+    \n2) Son iguales
+    \nOpcion = '''
+
+    while True:
+
+        try:
+
+            funcion_seleccionada = int(input(menu))
+
+            if funcion_seleccionada == 1:
+                
+                numero = int(input('Digite un numero: '))
+                eh_positivo(numero)
+
+            elif funcion_seleccionada == 2:
+
+                numero1 = int(input('Digite un numero: '))
+                numero2 = int(input('Digite otro numero: '))
+                son_iguales(numero1, numero2)
+
+
+
+        except Exception as e:
+
+            print('Erro: ', e)
 
 
 
 if __name__ == '__main__':
 
-    eh_positivo(5)
-
-    son_iguales(5, 4)
+    main()
 
