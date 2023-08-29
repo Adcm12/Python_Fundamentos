8e5ee0bd8b0684a3ae9626f5f652524c57bdfe80
Adrian Castillo <david.marcanao07@gmail.com>
2023-08-28 21:28:58 -0300

    upload

--- aulas_7_8/review.py
+++ aulas_7_8/review.py
@@ -47,7 +47,6 @@ def menor(numero1, numero2):
         else: 
             print("Los numeros son iguales, digite numero diferentes")
 
-
 def verifica_si_es_par(numero):
 
     if numero > 0:
@@ -63,6 +62,79 @@ def verifica_si_es_par(numero):
 
         print('El numero es impar')
 
+def mayor_de_la_lista(lista):
+
+    variable = 0
+
+    for i in lista:
+
+        if  i > variable:
+
+            variable = i
+
+    print(f'\nEl numero mayor de la lista es: {variable}')
+
+def cantidad_de_pares_en_lista(lista):
+
+    lista_par : int= []
+    lista_impar : int= []
+
+    for i in lista:
+
+        if i % 2 == 0:
+
+            lista_par.append(i)
+        
+        else: 
+            lista_impar.append(i)
+
+    print(f'\nLista de pares {lista_par}, cantidad: {len(lista_par)}')
+    print(f'\nLista de impares {lista_impar}, cantidad: {len(lista_impar)}')
+
+def menor_de_la_lista(lista):
+
+    variable = lista[0]
+
+    for i in lista:
+
+        if  i < variable:
+
+            variable = i
+
+    print(f'\nEl numero menor de la lista es: {variable}')
+
+
+def calculadora(numero1, numero2, opcion):
+
+    if opcion == 1:
+
+        resultado = numero1 + numero2
+        print(f'La suma es: {resultado}')
+
+        
+    elif opcion == 2:
+
+        resultado = numero1 - numero2
+        print(f'La resta es: {resultado}')
+
+    elif opcion == 3:
+
+        resultado = numero1 * numero2
+        print(f'La multiplicacion es: {resultado}')
+
+    elif opcion == 4:
+
+        resultado = numero1 / numero2
+        print(f'La division es: {resultado}')
+    
+    # elif opcion == 5:
+
+    #     quit()
+    
+    else:
+        print('Digite una opcion valida')
+
+
 
 def main():
 
@@ -73,6 +145,10 @@ def main():
     \n3) Maior
     \n4) Menor
     \n5) Es Par
+    \n6) Mayor de varios numeros
+    \n7) Cantidad de pares e impares
+    \n8) Menor de varios numeros
+    \n9) Calculadora
     \nOpcion = '''
 
     while True:
@@ -109,12 +185,63 @@ def main():
 
                 numero = int(input('\nDigite un numero: '))
                 verifica_si_es_par(numero)
+            
+            elif funcion_seleccionada == 6:
+
+                lista = []
+                numero = int(input('Cuantos numeros va a inserir en la lista?: '))
+
+                for i in range (numero):
+
+                    valor=int(input(f"Ingrese el elemento {(i+1)}: "))
+                    lista.append(valor)
+                
+                mayor_de_la_lista(lista)
+            
+            elif funcion_seleccionada == 7:
+
+                lista = []
+                numero = int(input('Cuantos numeros va a inserir en la lista?: '))
+
+                for i in range (numero):
+
+                    valor=int(input(f"Ingrese el elemento {(i+1)}: "))
+                    lista.append(valor)
+                
+                cantidad_de_pares_en_lista(lista)
+
+            elif funcion_seleccionada == 8:
+
+                lista = []
+                numero = int(input('Cuantos numeros va a inserir en la lista?: '))
+
+                for i in range (numero):
+
+                    valor=int(input(f"Ingrese el elemento {(i+1)}: "))
+                    lista.append(valor)
+                
+                menor_de_la_lista(lista)
+
+            elif funcion_seleccionada == 9:
+
+                while True:
+
+                    try:
+                        opcion = int(input('\nEscoja una opcion \n1) Sumar \n2)Restar \n3)Multiplicar \n4)Dividir \n5)Break \n: '))
+                        
+                        if opcion == 5:
+
+                            break
+                        else:    
+                            numero1 = int(input('\nDigite un numero: '))
+                            numero2 = int(input('Digite otro numero: '))
+                            calculadora(numero1, numero2, opcion)
+
+                    except Exception as e:
+                        print('Erro: ', e)
 
         except Exception as e:
-
             print('Erro: ', e)
-
-
 
 if __name__ == '__main__':
 
